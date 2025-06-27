99ca6a1a2f46676da13b9be63353f9193352ebe6
labete <emmanuelemejuru15@gmail.com>
2025-06-27 13:08:14 +0100

    chore: initialize NestJS project

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { typeOrmConfig } from 'typeorm.config';
+
+@Module({
+  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
